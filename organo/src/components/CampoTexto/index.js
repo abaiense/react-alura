e348b9b36871465b3ad67d6801f65109b806c65d
abaiense <antonio.baiense@gmail.com>
2022-10-14 12:42:35 -0300

    usando o useState para guardar o estado da informacao

--- organo/src/components/CampoTexto/index.js
+++ organo/src/components/CampoTexto/index.js
@@ -1,13 +1,21 @@
+import {useState} from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
     
     const placeholderModificada = `${props.placeholder}...`
+
+    const [valor, setValor] = useState('Guilherme Silveira')
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+        console.log(valor);
+    }
     
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} type="text" placeholder={placeholderModificada}/>
+            <input onChange={aoDigitado} value={props.valor}  required={props.obrigatorio} type="text" placeholder={placeholderModificada}/>
         </div>
     );
 }
--- organo/src/components/Formulario/index.js
+++ organo/src/components/Formulario/index.js
@@ -1,33 +1,61 @@
+import { useState } from "react";
 import "./Formulario.css";
-import CampoTexto from '../CampoTexto'
+import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
 
 const Formulario = () => {
-
   const times = [
-    'Programação',
-    'Front-end',
-    'Data Science',
-    'Devops',
-    'UX  e Design',
-    'Mobila',
-    'Inovação e Gestão'
-  ]
+    "Programação",
+    "Front-end",
+    "Data Science",
+    "Devops",
+    "UX  e Design",
+    "Mobila",
+    "Inovação e Gestão",
+  ];
+
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
 
   const aoSalvar = (event) => {
     event.preventDefault();
-    console.log("o Form foi sumetido")
-  }
+    console.log("o Form foi sumetido", nome, cargo, imagem, time);
+  };
 
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereco da imagem" />
-        <ListaSuspensa obrigatorio={true} label="Times" itens={times}/>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite o endereco da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Times"
+          itens={times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
         <Botao>Criar Card</Botao>
       </form>
     </section>
