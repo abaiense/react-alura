f1724b0a904593155c130cf7d4f67ac570e44517
abaiense <antonio.baiense@gmail.com>
2022-10-14 11:32:33 -0300

    aula 02 - react

--- organo/src/components/Formulario/index.js
+++ organo/src/components/Formulario/index.js
@@ -1,14 +1,34 @@
 import "./Formulario.css";
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 
 const Formulario = () => {
+
+  const times = [
+    'Programação',
+    'Front-end',
+    'Data Science',
+    'Devops',
+    'UX  e Design',
+    'Mobila',
+    'Inovação e Gestão'
+  ]
+
+  const aoSalvar = (event) => {
+    event.preventDefault();
+    console.log("o Form foi sumetido")
+  }
+
   return (
     <section className="formulario">
-      <form action="">
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereco da imagem" />
+        <ListaSuspensa obrigatorio={true} label="Times" itens={times}/>
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
